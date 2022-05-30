514dd8c435943aab31186a8e7fb26a2d90c96681
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-30 18:16:33 +0530

    Update task3.c

--- task3.c
+++ task3.c
@@ -1,1 +1,19 @@
-// Q3) write a c program of enum 
+// Q3) write a c program of enum.
+
+include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO = 10, EBAY, MICROSOFT } ;
+
+   enum Company xerox = XEROX;
+   enum Company google = GOOGLE;
+   enum Company ebay = EBAY;
+
+   printf("The value of xerox is: %d\n", xerox);
+   printf("The value of google is: %d\n", google);
+   printf("The value of ebay is: %d\n", ebay);
+
+    return 0;
+}
